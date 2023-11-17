7487b918713491364513398fc1b25d664d1ef8b8
Martín Canevaro <martincanevaro@yahoo.com.ar>
2023-11-17 12:10:23 -0300

    Update package main.go
    fix code

--- retoKelvinToCelcius/package main.go
+++ retoKelvinToCelcius/package main.go
@@ -1,62 +1,17 @@
 package main
 
-import "fmt"
-
-type Car struct {
-	make  string
-	model string
-	color string
-	doors int
-}
-
-type CarBuilder interface {
-	SetMake(make string)
-	SetModel(model string)
-	SetColor(color string)
-	SetDoors(doors int)
-	Build() Car
-}
-
-type BasicCarBuilder struct {
-	car *Car
-}
-
-func (b *BasicCarBuilder) SetMake(make string) {
-	b.car.make = make
-}
-
-func (b *BasicCarBuilder) SetModel(model string) {
-	b.car.model = model
-}
-
-func (b *BasicCarBuilder) SetColor(color string) {
-	b.car.color = color
-}
-
-func (b *BasicCarBuilder) SetDoors(doors int) {
-	b.car.doors = doors
-}
-
-func (b *BasicCarBuilder) Build() Car {
-	return *b.car
-}
+import (
+    "fmt"
+)
 
 func main() {
-	builder := &BasicCarBuilder{
-		car: &Car{
-			make:  "Toyota",
-			model: "Corolla",
-			color: "Red",
-			doors: 4,
-		},
-	}
+    // Temperatura en Kelvin
+    var temperaturaKelvin float64 = 373.15
 
-	builder.SetMake("Honda")
-	builder.SetModel("Civic")
-	builder.SetColor("Blue")
-	builder.SetDoors(2)
+    // Calculamos la temperatura en Celsius
+    var temperaturaCelsius float64 = temperaturaKelvin - 273.15
 
-	car := builder.Build()
-
-	fmt.Println(car)
+    // Mostramos los resultados en pantalla
+    fmt.Println("Temperatura en Kelvin:", temperaturaKelvin)
+    fmt.Println("Temperatura en Celsius:", temperaturaCelsius)
 }
